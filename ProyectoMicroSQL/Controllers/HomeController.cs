ccc335c4d26803af2a63aa2020036c777b68bd27
Mario991012 <mvelasquezb99@gmail.com>
2019-04-30 00:35:57 -0600

    Arbol completo

---
+++ ProyectoMicroSQL/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoMicroSQL.Singleton;
+
+namespace ProyectoMicroSQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CodigoSQL()
+        {
+            return View();
+        }
+
+        static int contador = 0;
+        public ActionResult Carga()
+        {
+            if (contador > 0)
+            {
+                ViewBag.Msg = "ERROR AL CARGAR EL ARCHIVO, INTENTE DE NUEVO";
+            }
+            contador++;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Carga(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                Upload(file);
+                return RedirectToAction("Upload");
+
+            }
+            else
+            {
+                ViewBag.Msg = "ERROR AL CARGAR EL ARCHIVO, INTENTE DE NUEVO";
+                return View();
+            }
+        }
+
+        public ActionResult Upload (HttpPostedFileBase file)
+        {
+            string model = "";
+            if (file != null && file.ContentLength > 0)
+            {
+                model = Server.MapPath("~/Upload/") + file.FileName;
+                file.SaveAs(model);
+                Data.Instancia.LecturaCSV(model);
+                ViewBag.Msg = "Carga del archivo correcta";
+                return RedirectToAction("Menu"); //VERIFICAR
+            }
+            else
+            {
+                ViewBag.Msg = "ARCHIVO SIN CONTENIDO";
+                return RedirectToAction("Carga");
+            }
+        }
+
+        public ActionResult Menu()
+        {
+            ViewBag.Mensaje = "";
+            return View();
+        }
+
+        public ActionResult Personalizar()
+        {
+            return View(Data.Instancia.Dictionary);
+        }
+
+        public ActionResult Modificar(string NewWord)
+        {
+            try
+            {
+                ViewBag.Error = "";
+                string[] array = NewWord.Split(',');
+                string key = array[0].Trim();
+                string value = array[1].Trim();
+
+                Data.Instancia.Dictionary[key] = value;
+
+                return View("Personalizar");
+            }
+            catch
+            {
+                ViewBag.Error = "Mala escritura";
+                return View("Personalizar");
+            }
+            
+        }
+
+        public ActionResult Reestablecer()
+        {
+            Data.Instancia.Reestablecer();
+            ViewBag.Mensaje = "Restablecimiento completado";
+            return View("Menu");
+        }
+    }
+}
---
+++ ProyectoMicroSQL/Singleton/Data.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace ProyectoMicroSQL.Singleton
+{
+    public class Data
+    {
+        private static Data instancia = null;
+        public static Data Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Data();
+                }
+                return instancia;
+            }
+        }
+
+        public Dictionary<string, string> Dictionary = new Dictionary<string, string>();
+        public List<string> ListaV = new List<string>();
+        public List<string> ListaK = new List<string>();
+
+        public void LecturaCSV(string path)
+        {
+            string[] lineas = File.ReadAllLines(path);
+            var contador = 0;
+            foreach (var item in lineas)
+            {
+                if (contador > 0)
+                {
+                    string[] infolinea = item.Split(';');
+                    
+                    Dictionary.Add(infolinea[0], infolinea[1]);
+                    ListaK.Add(infolinea[0]);
+                    ListaV.Add(infolinea[1]);
+                }
+                else
+                {
+                    contador++;
+                }
+            }
+        }
+        public void Reestablecer()
+        {
+            for (int i = 0; i < ListaV.Count; i++)
+            {
+                Dictionary[ListaK[i]] = ListaV[i];
+              
+            }
+            
+        }
+    }
+}
